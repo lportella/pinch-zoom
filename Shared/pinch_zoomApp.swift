a09ad2c4333e9fbe5d20ade1be8af2b628a2edb2
Lucas Portella <lucas.portellasantos@gmail.com>
2022-09-12 18:03:12 -0300

    Initial commit

---
+++ Shared/pinch_zoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_zoomApp.swift
+//  Shared
+//
+//  Created by Lucas Portella on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct pinch_zoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
