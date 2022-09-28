5b34584422f369298e97ac244d7fe65a31bbf09a
Lucas Portella <lucas.portella@picpay.com>
2022-09-28 20:05:25 -0300

    Add default cover image

--- Shared/Screen/ContentView.swift
+++ Shared/Screen/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Properties
+    
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1.0), value: isAnimating)
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1.0)) {
+                    isAnimating = true
+                }
+            })
+        }
+        .navigationViewStyle(.stack)
     }
 }
 
