1d44fffffe2d677069745d8f093d146b9165b1e9
Lucas Portella <lucas.portella@picpay.com>
2022-09-29 17:05:19 -0300

    add info panel view

---
+++ Shared/View/InfoPanelView.swift
@@ -0,0 +1,25 @@
+//
+//  InfoPanelView.swift
+//  pinch-zoom (iOS)
+//
+//  Created by Lucas Portella on 29/09/22.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
